a0dbe5650c466e181f77aa86644f975311662b5f
Samuel Davi <samuel.davimont@gmail.com>
2022-03-04 14:16:39 -0300

    quiz inicial

---
+++ js/questions.js
@@ -0,0 +1,46 @@
+const questions = [
+    
+
+    {
+        question: 'JavaScript é uma linguagem de programação Orientada a Objetos?',
+        options:[
+            {option:'Sim' , value:true},
+            {option:'Não'},
+            {option:'JavaScript não é uma linguagem!'},
+
+        ]
+    },
+
+    {
+        question: 'Como você alteraria o estilo de um elemento?',
+        options:[
+            {option:'document.qwrtSelector("tag").style.color = "red"'},
+            {option:'document.querySelector("tag").style.color = "red"' , value:true},
+            {option:'document.QuerySelector("tag").style.color = "red"'},
+
+        ]
+    },
+
+    {
+        question: 'Qual a forma correta de se adicionar uma Classe a um elemento?',
+        options:[
+            {option:'elemento.classAdd = "classe"'},
+            {option:'elemento.class = "classe"}'},
+            {option:'elemento.classList.add("classe")', value:true},
+
+        ]
+    },
+    {
+        question: 'Oque está faltando para essa função funcionar?\n\n<pre>function (){\nconsole.log("Hello Word")\n}\n</pre>',
+        options:[
+            {option:'Retirar a palavra Function'},
+            {option:'Adiconar um nome à função' , value:true},
+            {option:'Remover as aspas'},
+
+        ]
+    }
+
+
+    
+]
+
---
+++ js/script.js
@@ -0,0 +1,134 @@
+const qs = (e)=>{
+    return document.querySelector(e)
+}
+
+const qsall = (e)=>{
+    return document.querySelectorAll(e)
+}
+
+let letters = 'abcdefghijklmnopqrstuvwxyz'
+
+let currentQuestion = 0
+
+let hits = 0
+let mistakes = 0
+let blocked = false
+
+const selectOption = {
+
+    verify(){
+        let idQuestion = this.dataset.idQuestion
+        let idOption = this.dataset.idOption
+
+        let localVerify = questions[idQuestion].options[idOption]
+
+        if(!blocked){
+            if(localVerify.value == true){
+                console.log('acertou')
+                hits ++
+            }else{
+                console.log('errou')
+                mistakes ++
+            }
+    
+            currentQuestion ++
+            handleQuestion()
+        }
+       
+    },
+
+    calculateResult(){
+        blocked = true
+        let numQuestions = questions.length
+        let percent = (hits / numQuestions) * 100
+
+        let msg = ''
+        let color = ''
+
+        if(percent == 100){
+            msg = 'Mestre! Excelente!'
+            color = '#08dd08'
+        }else if(percent >= 80){
+            msg = 'Excelente! Parabéns!'
+            color = 'gold'
+        }else if(percent >= 50){
+            msg = 'Ótimo! Muito Bom!'
+            color = 'blue'
+        }else if(percent >= 30){
+            msg = 'Quase lá'
+            color = 'orange'
+        }else {
+            msg = 'Não foi dessa Vez!'
+            color = 'red'
+        }
+
+        qs('.result .msg').innerHTML = msg
+        
+        qs('.result .percent').innerHTML = `Acertou ${percent}%`
+        qs('.result .percent').style.color = color
+
+        qs('.result .hits').innerHTML = `Você acertou ${hits} de ${numQuestions} questões`
+        
+
+        qs('.result').style.opacity = '0'
+        setTimeout(()=>{
+            qs('.result').style.opacity = '1'
+        },300)
+
+        qs('.result').style = 'display: block !important;'
+
+        qs('.result button').onclick = selectOption.reset
+
+    },
+
+    reset(){
+        blocked = false
+        currentQuestion = 0
+        hits = 0
+        mistakes = 0
+
+        qs('.result').style.opacity = '0'
+        setTimeout(()=>{
+            qs('.result').style.display = 'none'
+        },300)
+        handleQuestion()
+    
+    }
+}
+
+let handleQuestion = ()=>{
+
+    if(questions[currentQuestion]){
+
+        qs('.container-options').innerHTML = ''
+        
+        let question = questions[currentQuestion]
+
+        qs('.question .question-title').innerHTML = `Questão ${currentQuestion + 1}`
+
+        qs('.question .question-value').innerHTML = question.question
+
+        question.options.forEach((option,index)=>{
+
+            let tagOption = qs('.models .option').cloneNode(true)
+
+            tagOption.querySelector('.value-option').innerHTML = option.option
+
+            tagOption.querySelector('.letter').innerHTML = letters[index].toUpperCase()
+
+            tagOption.dataset.idQuestion = currentQuestion
+            tagOption.dataset.idOption = index
+
+            tagOption.addEventListener('click',selectOption.verify)
+            
+            qs('.container-options').append(tagOption)
+        })
+    }else{
+        selectOption.calculateResult()
+    }
+   
+}
+
+handleQuestion()
+
+
